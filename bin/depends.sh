01c6d00c8608bf17f496aad171833f622d418533
Ian Dennis Miller <ian@iandennismiller.com>
2019-07-27 18:21:23 -0400

    update documentation

--- bin/depends.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-# project_system (c) Ian Dennis Miller
-
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv -a . -r requirements-dev.txt project_system
-source ~/.virtualenvs/project_system/bin/activate

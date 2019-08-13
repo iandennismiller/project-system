750fcd2f2190cf1869e244e20d9389facab79c07
Ian Dennis Miller <ian@iandennismiller.com>
2019-08-13 10:41:24 -0400

    streamline configuration include virtualenvwrapper config

--- project_system/__meta__.py
+++ project_system/__meta__.py
@@ -2,7 +2,7 @@
 # project-system (c) Ian Dennis Miller
 
 __project__ = 'project-system'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Ian Dennis Miller'
 __email__ = 'iandennismiller@gmail.com'
 __url__ = 'http://project-system.readthedocs.io'
